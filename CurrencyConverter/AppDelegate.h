846294cd24dc8b133a6fa08ab5eab64585e2118c
Daniela Guillen <deg2158@columbia.edu>
2016-07-12 11:00:33 -0400

    Initial Commit

---
+++ CurrencyConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyConverter
+//
+//  Created by Daniela Guillen on 7/12/16.
+//  Copyright © 2016 Daniela Guillen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrencyConverter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CurrencyConverter
+//
+//  Created by Daniela Guillen on 7/12/16.
+//  Copyright © 2016 Daniela Guillen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
